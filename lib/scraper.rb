1e0baca78367db225e714be8048da559d52d60e1
julianxlozano <julianxlozano@gmail.com>
2021-03-10 13:50:37 -0600

    commit tree 2cf627c8ec6bb18edcddb23c5381978789d13481

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,18 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html) 
+doc.css(".headline-26OIBN").text.strip 
+
+courses = doc.css(".title-oE5vT4")
+
+courses.each do |course|
+    if course.text.strip  == "Software Engineering" ||
+        course.text.strip  ==  "Data Science"||
+        course.text.strip  ==  "Cybersecurity Analytics"|| 
+        course.text.strip  ==  "Cybersecurity Engineering"
+    puts course.text.strip 
+    end 
+end
+
